2ee56e62e8fb60492fe933bc36cdfeded9aa8698
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-03-02 21:06:39 +0700

    Library

---
+++ P3_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+float Luas, d1, d2;
+
+void ProsedurInputData(){
+    cout << "Masukan diagonal 1 : ";
+    cin >> d1;
+    cout << "Masukan diagonal 2 : ";
+    cin >> d2;
+}
