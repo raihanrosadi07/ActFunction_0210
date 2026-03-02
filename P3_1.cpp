e8bf86a703373335b63817bf30dfc8d503dc8a53
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-03-02 21:13:39 +0700

    ProsesInputData

--- P3_1.cpp
+++ P3_1.cpp
@@ -8,3 +8,12 @@ void ProsedurInputData(){
     cout << "Masukan diagonal 2 : ";
     cin >> d2;
 }
+
+float HitungLuasBerparameter(float d1, float d2)
+{
+    return 0,5 * d1 * d2;
+}
+
+void ProsedurOutput(){
+    cout << "Luas Ketupat : " << HitungLuasBerparameter(d1, d2);
+}
